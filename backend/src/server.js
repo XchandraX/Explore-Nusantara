ae9cf73837fe239cc64acbaf09e8be55275bc472
chandrx <146406701+chandrx@users.noreply.github.com>
2025-03-23 12:16:27 +0700

    sementara

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,11 @@
-require('dotenv').config(); // Panggil dotenv di awal agar bisa membaca .env
+require('dotenv').config(); 
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('hapi-auth-jwt2');
 const userRoutes = require('./routes/userRoutes');
 const wisataRoutes = require('./routes/wisataRoutes');
 const eventRoutes = require('./routes/eventRoutes');
-const ratingRoutes = require('./routes/ratingRoutes'); // Tambahkan route rating
-const mlRoutes = require('./routes/mlRoutes');
+const ratingRoutes = require('./routes/ratingRoutes'); 
 
 const validate = async (decoded, request, h) => {
   return { isValid: true };
@@ -29,9 +28,9 @@ const init = async () => {
     verifyOptions: { algorithms: ['HS256'] }
   });
 
-  server.route([...userRoutes, ...wisataRoutes, ...eventRoutes, ...ratingRoutes, ...mlRoutes]);
+  server.route([...userRoutes, ...wisataRoutes, ...eventRoutes, ...ratingRoutes]);
 
-  console.log("JWT Secret:", process.env.JWT_SECRET); // Pastikan ini dipanggil setelah dotenv
+  console.log("JWT Secret:", process.env.JWT_SECRET); 
 
   await server.start();
   console.log(`✅ Server berjalan di ${server.info.uri}`);
