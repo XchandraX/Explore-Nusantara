edfa4a582635309e180c7b5e62db0e59c3b8c39c
nayaaasss <widamud0323@gmail.com>
2025-04-07 13:28:46 +0700

    responsif file destinasi detail information, budaya landing page, and budaya detail information

--- frontend/src/script/index.js
+++ frontend/src/script/index.js
@@ -11,18 +11,5 @@ menuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
 
-// untuk membuat hover menetap
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("page-btn")) {
-      const buttons = document.querySelectorAll(".page-btn");
-  
-      buttons.forEach(btn => {
-        btn.classList.remove("bg-[#ac654a]", "text-white");
-        btn.classList.add("text-gray-500");
-      });
-  
-      e.target.classList.remove("text-gray-500");
-      e.target.classList.add("bg-[#ac654a]", "text-white");
-    }
-  });
+
 
